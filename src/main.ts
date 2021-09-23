730b69c4fe130b9cde4e047918ead7fa8237b097
Mohamed Hamada <mohamed43628@gmail.com>
2021-09-23 12:55:39 +0200

    added typescript and basic src/main.ts

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+
+const app: Application = express();
+
+const sayHello = (req: Request, res: Response) => {
+  res.send("Hello from server");
+};
+
+app.get("/status", sayHello);
+
+app.listen(5000, () => {
+  console.log("We are up@running");
+});
